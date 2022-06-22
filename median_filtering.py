0a60201eb567cfbc0c227fe619d76600dfcaef3b
Bruno Schuty <56778839+bschuty@users.noreply.github.com>
2022-06-22 15:46:32 -0300

    final version

--- median_filtering.py
+++ median_filtering.py
@@ -9,11 +9,10 @@ filt_time = np.arange(6)
 arr_g_psrn = []
 arr_s_psrn = []
 for i in range(3):
-    route_16avg = str('/home/bruno/Documentos/TESIS/TESIS/filtrado/16avg/')
+    route_16avg = str('/home/bruno/Documentos/Proyectos/TESIS/MIP/data/filtering/')
     file_16avg = route_16avg + str(i + 1) + str('_16avg_3x3.lsm')
     im_16avg = tifffile.imread(file_16avg)
-
-    route_2avg = str('/home/bruno/Documentos/TESIS/TESIS/filtrado/2avg/')
+    route_2avg = str('/home/bruno/Documentos/Proyectos/TESIS/MIP/data/filtering/')
     file_2avg = route_2avg + str(i + 1) + str('_2avg_3x3.lsm')
     im_2avg = tifffile.imread(file_2avg)
 
@@ -37,30 +36,23 @@ for i in range(3):
 arr_g_psrn = np.asarray(arr_g_psrn)
 arr_s_psrn = np.asarray(arr_s_psrn)
 
-plt.figure(1)
+fig, ax = plt.subplots(2, figsize=(10, 5))
 for i in range(len(arr_g_psrn)):
-    plt.plot(filt_time, arr_g_psrn[i], '-x')
-plt.title('PSNR(filtering times) for G')
-plt.xlabel('Times (0 ; 5)')
-plt.ylabel('PSNR[dB]')
-plt.grid()
+    ax[0].plot(filt_time, arr_g_psrn[i], '-x')
+    ax[1].plot(filt_time, arr_s_psrn[i], '-x')
 
-plt.figure(2)
-for i in range(len(arr_s_psrn)):
-    plt.plot(arr_s_psrn[i], '-x')
-plt.title('PSNR(filtering times) for S')
+ax[0].set_title('PSNR for G')
+ax[1].set_title('PSNR for S')
+ax[0].set_ylabel('PSNR[dB]')
+ax[1].set_ylabel('PSNR[dB]')
 plt.xlabel('Times (0 ; 5)')
-plt.ylabel('PSNR[dB]')
-plt.grid()
-
-plt.show()
 
 prueba = True
 if prueba:
     # regiones interesantes img 2 indice 3, 4 y 5
     optimal = 2
-    im_2avg = tifffile.imread(str('/home/bruno/Documentos/TESIS/TESIS/filtrado/2avg/2_2avg_3x3.lsm'))
-    im_16avg = tifffile.imread(str('/home/bruno/Documentos/TESIS/TESIS/filtrado/16avg/2_16avg_3x3.lsm'))
+    im_2avg = tifffile.imread(str('/home/bruno/Documentos/Proyectos/TESIS/MIP/data/filtering/1_2avg_3x3.lsm'))
+    im_16avg = tifffile.imread(str('/home/bruno/Documentos/Proyectos/TESIS/MIP/data/filtering/1_16avg_3x3.lsm'))
     ind = 4
     dc_optimal, g_optimal, s_optimal, _, _ = phlib.phasor(im_16avg[ind])
     dc, g, s, _, _ = phlib.phasor(im_2avg[ind])
@@ -83,8 +75,8 @@ if prueba:
 
 # muestro como quedan con varias veces filtrados
 if True:
-    im_2avg = tifffile.imread(str('/home/bruno/Documentos/TESIS/TESIS/filtrado/2avg/2_2avg_3x3.lsm'))
-    im_16avg = tifffile.imread(str('/home/bruno/Documentos/TESIS/TESIS/filtrado/16avg/2_16avg_3x3.lsm'))
+    im_2avg = tifffile.imread(str('/home/bruno/Documentos/Proyectos/TESIS/MIP/data/filtering/2_2avg_3x3.lsm'))
+    im_16avg = tifffile.imread(str('/home/bruno/Documentos/Proyectos/TESIS/MIP/data/filtering/2_16avg_3x3.lsm'))
     ind = 4
     dc_optimal, g_optimal, s_optimal, _, _ = phlib.phasor(im_16avg[ind])
     dc, g, s, _, _ = phlib.phasor(im_2avg[ind])
@@ -117,7 +109,7 @@ if True:
     dc = [dc, dc, dc]
     g = [g_filt2, g_filt3, g_filt4]
     s = [s_filt2, s_filt3, s_filt4]
-    ic = [2, 2, 2]
+    ic = [1, 1, 1]
     titles = ['Filtered: twice', 'Filtered: 3 times', 'Filtered: 4 times']
     phlib.phasor_plot(dc, g, s, ic, title=titles)
     plt.show()

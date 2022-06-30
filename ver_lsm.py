e1e7aa0565fef494adcde163e26c8f1ef69e02d9
Bruno Schuty <56778839+bschuty@users.noreply.github.com>
2022-06-30 13:55:01 -0300

    Update ver_lsm.py

--- ver_lsm.py
+++ ver_lsm.py
@@ -3,16 +3,17 @@ import tifffile
 import matplotlib.pyplot as plt
 
 
-file = str('/home/bruno/Documentos/Proyectos/TESIS/TESIS/base de datos lsm/2021/20338_SP_Tile_6x4_pos_2a.lsm')
-m = 6
-n = 4
+file = str('/home/bruno/Documentos/Proyectos/TESIS/TESIS/base de datos lsm/2022/MELANOMAS/scaneo lineal/HE-Melanomas/19507_SP_Tile_5x5.lsm')
+m = 5
+n = 5
 im = tifffile.imread(file)
 dc, g, s, _, _ = phlib.phasor_tile(im, 1024, 1024)
-dc = phlib.concatenate(dc, n, m, bidirectional=True)
-# g = phlib.concatenate(g, n, m, bidirectional=True)
-# s = phlib.concatenate(s, n, m, bidirectional=True)
-# phlib.interactive(dc, g, s, 0.2)
+dc = phlib.concatenate(dc, n, m)
+g = phlib.concatenate(g, n, m)
+s = phlib.concatenate(s, n, m)
+phlib.interactive(dc, g, s, 0.2)
 
 plt.figure(1)
-plt.imshow(dc, cmap='nipy_spectral')
+plt.imshow(dc, cmap='gray')
+plt.axis('off')
 plt.show()

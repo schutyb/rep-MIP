a8dfdf9716911da4aa8ea2d2f257b32b62fb63e4
Bruno Schuty <56778839+bschuty@users.noreply.github.com>
2022-06-30 13:55:30 -0300

    Update create_ometiff.py

--- create_ometiff.py
+++ create_ometiff.py
@@ -4,12 +4,12 @@ from skimage.filters import median, gaussian
 import matplotlib.pyplot as plt
 import numpy as np
 
-file = str('/home/bruno/Documentos/Proyectos/TESIS/MIP/data/prueba/15410_SP_Tile_4x4.lsm')
+file = str('/home/bruno/Documentos/Proyectos/TESIS/TESIS/base de datos lsm/comp_HE/HE-Melanomas/16952_SP_Tile_4x3.lsm')
 im = tifffile.imread(file)
 dc, g, s, _, _ = phlib.phasor_tile(im, 1024, 1024)
 
 m = 4
-n = 4
+n = 3
 dc = phlib.concatenate(dc, n, m)
 g = phlib.concatenate(g, n, m)
 s = phlib.concatenate(s, n, m)
@@ -22,15 +22,15 @@ while cont < 2:
     cont = cont + 1
 
 # umbralización
-dc = np.where(dc > 2, dc, np.zeros(dc.shape))
-g = np.where(dc > 2, g, np.zeros(g.shape))
-s = np.where(dc > 2, s, np.zeros(s.shape))
+dc = np.where(dc > 1, dc, np.zeros(dc.shape))
+g = np.where(dc > 1, g, np.zeros(g.shape))
+s = np.where(dc > 1, s, np.zeros(s.shape))
 ph = np.round(np.angle(g + s * 1j, deg=True))
 md = np.sqrt(g ** 2 + s ** 2)
 
 # guardo los datos en un ome.tiff
-filename = '/home/bruno/Documentos/Proyectos/TESIS/MIP/data/prueba/prueba.ome.tiff'
-data = phlib.generate_file(filename, [dc, g, s, ph, md])
+filename = '/home/bruno/Documentos/Proyectos/TESIS/TESIS/base de datos lsm/comp_HE/HE-Melanomas/ometiff/16952.ome.tiff'
+data = phlib.generate_file(filename, [dc, g, s, md, ph])
 
 '''fig, ax = plt.subplots(1, 2, figsize=(20, 12))
 ax[0].imshow(ph, cmap='gray', interpolation='none')
